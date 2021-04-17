7f3bb4e7c35a0b1a4d690ba57039aaa9c21b4750
Vincius <viniciustprates@gmail.com>
2021-04-17 20:43:58 -0300

    :package: added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';;
+import App from './App';
+
+import {AuthProvider} from './contexts/authContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
